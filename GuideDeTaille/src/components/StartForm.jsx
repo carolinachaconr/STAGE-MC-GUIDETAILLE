0514a2fb2f5f48c1500f99ae5649697d00e84020
carolinachaconr <elizabethcarolina.chrz@gmail.com>
2026-05-13 08:55:14 -0400

    Update StartForm.jsx

--- GuideDeTaille/src/components/StartForm.jsx
+++ GuideDeTaille/src/components/StartForm.jsx
@@ -14,7 +14,7 @@ export default function StartForm() {
         {!startQuiz ? (
           <>
             <h1 className="text-2xl font-bold mb-4">Bienvenue dans le guide de taille</h1>
-            <p>Avez-vous besoin d'aide pour choisir votre taille ?</p>
+            <p className="pb-30">Avez-vous besoin d'aide pour choisir votre taille ?</p>
             <button 
             onClick={() => setStartQuiz(true)}
             className="w-full bg-[#d71e3e] text-white py-2 rounded-lg hover:bg-[#b01a30]">
